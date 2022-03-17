01714633e0def15ffa7b24693021c8c30fb8006f
wayao <wayao@vmware.com>
2022-03-17 10:39:43 +0800

    add test of execsort_sort_bounded_heap

--- src/test/regress/sql/query_finish_pending.sql
+++ src/test/regress/sql/query_finish_pending.sql
@@ -24,6 +24,15 @@ select DISTINCT S from (select row_number() over(partition by i1 order by i2) AS
 
 select gp_inject_fault('execsort_dumptuples', 'status', 2);
 
+select gp_inject_fault('execsort_sort_bounded_heap', 'reset', 2);
+-- set QueryFinishPending=true in sort_bounded_heap. This will stop sort.
+select gp_inject_fault('execsort_sort_bounded_heap', 'finish_pending', 2);
+
+-- return results although sort will be interrupted in one of the segments 
+select S from (select row_number() over(partition by i1 order by i2) AS T, count(*) over (partition by i1) AS S from _tmp_table) AS TMP order by S limit 3;
+
+select gp_inject_fault('execsort_sort_bounded_heap', 'status', 2);
+
 -- test if shared input scan deletes memory correctly when QueryFinishPending and its child has been eagerly freed,
 -- where the child is a Sort node
 drop table if exists testsisc;
@@ -65,6 +74,7 @@ select gp_inject_fault('execshare_input_next', 'status', 2);
 -- Disable faultinjectors
 select gp_inject_fault('execsort_sort_mergeruns', 'reset', 2);
 select gp_inject_fault('execsort_dumptuples', 'reset', 2);
+select gp_inject_fault('execsort_sort_bounded_heap', 'reset', 2);
 select gp_inject_fault('execshare_input_next', 'reset', 2);
 
 -- test if a query can be canceled when cancel signal arrives fast than the query dispatched.
